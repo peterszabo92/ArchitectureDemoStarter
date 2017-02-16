d3b69eea3def45d5b13728ef64786d9b10221b23
Peter Szabo <peter.szabo@hipteam.io>
2017-02-16 12:37:34 +0100

    General approach to set picture ratio

--- app/src/main/java/com/example/mvvmdemo/gallery/viewholder/PictureListItemHolder.java
+++ app/src/main/java/com/example/mvvmdemo/gallery/viewholder/PictureListItemHolder.java
@@ -2,6 +2,7 @@ package com.example.mvvmdemo.gallery.viewholder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -10,7 +11,6 @@ import com.example.mvvmdemo.BaseApplication;
 import com.example.mvvmdemo.R;
 import com.example.mvvmdemo.data.model.ImageModel;
 import com.example.mvvmdemo.util.AppConstants;
-import com.example.mvvmdemo.util.DimensionUtil;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,23 +23,35 @@ public class PictureListItemHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.picture)
     ImageView picture;
 
+    @BindView(R.id.picture_container)
+    FrameLayout pictureContainer;
+
+    private int pictureWidth = -1;
+    private int pictureHeight = -1;
+
     public PictureListItemHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
-        setItemAspectRatio(itemView);
+        calcPreviewSize();
     }
 
     public void update(ImageModel imageModel) {
+        setPreviewAspectRatio();
         pictureName.setText(imageModel.getName());
         BaseApplication.imageLoader.loadSimpleImageFromUrl(picture, imageModel.getImageUrl());
     }
 
-    private void setItemAspectRatio(View itemView) {
-        int margin = itemView.getResources().getDimensionPixelSize(R.dimen.picture_item_space);
-        int cellWidth = (int) ((DimensionUtil.getScreenWidth(itemView.getContext()) * 0.4) - (2 * margin));
-        int cellHeight = (int) (cellWidth * AppConstants.RATIO_916);
+    private void calcPreviewSize() {
+        pictureContainer.post(() -> {
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) pictureContainer.getLayoutParams();
+            pictureWidth = pictureContainer.getWidth();
+            pictureHeight = (int) (pictureWidth * AppConstants.RATIO_916);
+            params.height = pictureHeight;
+            pictureContainer.setLayoutParams(params);
+        });
+    }
 
-        LinearLayout.LayoutParams pictureParams = new LinearLayout.LayoutParams(cellWidth, cellHeight);
-        picture.setLayoutParams(pictureParams);
+    private void setPreviewAspectRatio() {
+        pictureContainer.setLayoutParams(new LinearLayout.LayoutParams(pictureWidth, pictureHeight));
     }
 }
